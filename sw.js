58b2a0f41d270c473f283b2a088750e26f0c74f6
Walter Jairzinho Sosa <waltersosa@gmail.com>
2026-04-20 15:30:25 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker minimalista para habilitar la instalación PWA
-const CACHE_NAME = 'vocesApp-v0.92';
+const CACHE_NAME = 'vocesApp-v0.4';
 const ASSETS = [
   './',
   './index.html',
