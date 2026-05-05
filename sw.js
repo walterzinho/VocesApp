d126e1e55ca75ab73f008b3251f495a305723607
Walter Jairzinho Sosa <waltersosa@gmail.com>
2026-05-05 14:56:09 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// --- VERSIÓN 0.90 - Actualización Forzada y Estabilidad ---
-const CACHE_NAME = 'voces-campesinas-v0.90';
+// --- VERSIÓN 0.91 - Actualización Forzada y Estabilidad ---
+const CACHE_NAME = 'voces-campesinas-v0.91';
 
 const ASSETS = [
   './',
